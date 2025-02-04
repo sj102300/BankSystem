84d7465dfe3bd343afb47eece8adb7902d80a0f8
seungJoo <sj10230506@gmail.com>
2025-02-04 12:30:47 +0900

    feat: 입출금 자기 계좌에만 가능하도록

--- src/db/bank/models/accountdb.cc
+++ src/db/bank/models/accountdb.cc
@@ -113,7 +113,7 @@ std::vector<Account> AccountDB::GetAccountsByCusId(int cusId)
         // 조회 결과가 비어있는 경우 빈 벡터 반환
         if (ret.empty())
             return {};
-        
+
         return ret;
     }
     catch (std::exception &e)
@@ -122,7 +122,7 @@ std::vector<Account> AccountDB::GetAccountsByCusId(int cusId)
     }
 }
 
-bool AccountDB::DepositBalanceByAccId(std::string& accId,
+bool AccountDB::DepositBalanceByAccId(std::string &accId,
                                       unsigned int deposit_amount, int cusId)
 {
     BankDatabase *db = BankDatabase::getInstance();
@@ -131,9 +131,12 @@ bool AccountDB::DepositBalanceByAccId(std::string& accId,
         auto accounts =
             db->getStorage().get_all<Account>(where(c(&Account::accId) == accId));
 
-        //조회할 계좌가 없음. 입금 실패
+        // 조회할 계좌가 없음. 입금 실패
         if (accounts.empty())
             return false;
+
+        if (accounts[0].cusId != cusId)
+            return false; // 권한없음. 입금 실패
 
         accounts[0].balance += deposit_amount;
         db->getStorage().update(accounts[0]);
@@ -146,7 +149,7 @@ bool AccountDB::DepositBalanceByAccId(std::string& accId,
     }
 }
 
-bool AccountDB::WithdrawBalanceByAccId(std::string& accId,
+bool AccountDB::WithdrawBalanceByAccId(std::string &accId,
                                        unsigned int withdraw_amount,
                                        int cusId)
 {
@@ -156,12 +159,15 @@ bool AccountDB::WithdrawBalanceByAccId(std::string& accId,
         auto targetAcc =
             db->getStorage().get_all<Account>(where(c(&Account::accId) == accId));
 
-        if(targetAcc.empty())
-            return false; //계좌 조회 실패. 출금 실패.
+        if (targetAcc.empty())
+            return false; // 계좌 조회 실패. 출금 실패.
 
         if (targetAcc[0].balance < withdraw_amount)
-            return false;  //잔액 부족. 출금 실패
-        
+            return false; // 잔액 부족. 출금 실패
+
+        if (targetAcc[0].cusId != cusId)
+            return false; // 권한 없음. 출금 실패
+
         targetAcc[0].balance -= withdraw_amount;
         db->getStorage().update(targetAcc[0]);
         MakeWithdrawLog(targetAcc[0], withdraw_amount);
@@ -188,15 +194,15 @@ void AccountDB::UpdateAccount(Account &targetAcc)
 
 void AccountDB::DeleteAccount(std::string accId)
 {
-    //transaction 외래키와의 종속성 때문에
-    //그냥 remove안하고 계좌 status를 false로
+    // transaction 외래키와의 종속성 때문에
+    // 그냥 remove안하고 계좌 status를 false로
     BankDatabase *db = BankDatabase::getInstance();
     try
     {
         auto targetAcc = db->getStorage().get_all<Account>(where(c(&Account::accId) == accId));
-        
-        if(targetAcc.empty())
-            return; //삭제할 계좌 없음.
+
+        if (targetAcc.empty())
+            return; // 삭제할 계좌 없음.
 
         targetAcc[0].status = false;
         db->getStorage().update(targetAcc[0]);
