8951b791b1d742b42d61b2c8ca86d09c248f87a3
seungJoo <sj10230506@gmail.com>
2025-02-04 13:12:16 +0900

    modify: try-catch 삭제

--- src/db/bank/models/accountdb.cc
+++ src/db/bank/models/accountdb.cc
@@ -23,130 +23,88 @@ void AccountDB::MakeWithdrawLog(Account &targetAcc,
                                 unsigned int withdraw_money)
 {
     BankDatabase *db = BankDatabase::getInstance();
-    try
-    {
-        std::string createdAt = TimeStamp::get_current_time();
-        db->getStorage().insert<TransactionLog>(
-            TransactionLog{0, targetAcc.id, targetAcc.accId, targetAcc.cusId, 2,
-                           withdraw_money, targetAcc.balance, createdAt});
-    }
-    catch (std::exception &e)
-    {
-        db->handleException(e);
-    }
+    std::string createdAt = TimeStamp::get_current_time();
+    db->getStorage().insert<TransactionLog>(
+        TransactionLog{0, targetAcc.id, targetAcc.accId, targetAcc.cusId, 2,
+                       withdraw_money, targetAcc.balance, createdAt});
 }
 
 void AccountDB::MakeDepositLog(Account &targetAcc, unsigned int deposit_money)
 {
     BankDatabase *db = BankDatabase::getInstance();
-    try
-    {
-        std::string createdAt = TimeStamp::get_current_time();
-        db->getStorage().insert<TransactionLog>(
-            TransactionLog{0, targetAcc.id, targetAcc.accId, targetAcc.cusId, 1,
-                           deposit_money, targetAcc.balance, createdAt});
-    }
-    catch (std::exception &e)
-    {
-        db->handleException(e);
-    }
+    std::string createdAt = TimeStamp::get_current_time();
+    db->getStorage().insert<TransactionLog>(
+        TransactionLog{0, targetAcc.id, targetAcc.accId, targetAcc.cusId, 1,
+                       deposit_money, targetAcc.balance, createdAt});
 }
 
 bool AccountDB::isExistAccId(std::string &accId)
 {
     BankDatabase *db = BankDatabase::getInstance();
-    try
-    {
-        return db->getStorage().count<Account>(where(c(&Account::accId) == accId));
-    }
-    catch (std::exception &e)
-    {
-        db->handleException(e);
-    }
+    return db->getStorage().count<Account>(where(c(&Account::accId) == accId));
 }
 
 Account AccountDB::CreateAccount(int cusId, unsigned int account_type)
 {
     BankDatabase *db = BankDatabase::getInstance();
-    try
+    // db에 없는 새로운 계좌번호 생성
+    std::string accId;
+    do
     {
-        // db에 없는 새로운 계좌번호 생성
-        std::string accId;
-        do
-        {
-            accId = std::to_string(GenerateRandomNumber());
-        } while (isExistAccId(accId));
+        accId = std::to_string(GenerateRandomNumber());
+    } while (isExistAccId(accId));
 
-        // 현재 시간을 string으로 변환
-        std::string createdAt = TimeStamp::get_current_time();
+    // 현재 시간을 string으로 변환
+    std::string createdAt = TimeStamp::get_current_time();
 
-        // Account 객체 생성 및 데이터베이스에 삽입
-        int insertedId = db->getStorage().insert(Account{
-            0,            // id (자동 생성)
-            accId,        // 계좌 ID
-            cusId,        // 고객 ID
-            0,            // 초기 잔액
-            account_type, // 계좌 유형
-            true,         // 활성화 상태
-            createdAt     // 생성 시간
-        });
+    // Account 객체 생성 및 데이터베이스에 삽입
+    int insertedId = db->getStorage().insert(Account{
+        0,            // id (자동 생성)
+        accId,        // 계좌 ID
+        cusId,        // 고객 ID
+        0,            // 초기 잔액
+        account_type, // 계좌 유형
+        true,         // 활성화 상태
+        createdAt     // 생성 시간
+    });
 
-        // 삽입된 계좌 정보를 데이터베이스에서 조회
-        Account insertedAccount = db->getStorage().get<Account>(insertedId);
-        return insertedAccount;
-    }
-    catch (std::exception &e)
-    {
-        db->handleException(e); // 예외 처리
-    }
+    // 삽입된 계좌 정보를 데이터베이스에서 조회
+    Account insertedAccount = db->getStorage().get<Account>(insertedId);
+    return insertedAccount;
 }
 
 std::vector<Account> AccountDB::GetAccountsByCusId(int cusId)
 {
     BankDatabase *db = BankDatabase::getInstance();
 
-    try
-    {
-        std::vector<Account> ret =
-            db->getStorage().get_all<Account>(where(c(&Account::cusId) == cusId));
+    std::vector<Account> ret =
+        db->getStorage().get_all<Account>(where(c(&Account::cusId) == cusId));
 
-        // 조회 결과가 비어있는 경우 빈 벡터 반환
-        if (ret.empty())
-            return {};
+    // 조회 결과가 비어있는 경우 빈 벡터 반환
+    if (ret.empty())
+        return {};
 
-        return ret;
-    }
-    catch (std::exception &e)
-    {
-        db->handleException(e);
-    }
+    return ret;
 }
 
 bool AccountDB::DepositBalanceByAccId(std::string &accId,
                                       unsigned int deposit_amount, int cusId)
 {
     BankDatabase *db = BankDatabase::getInstance();
-    try
-    {
-        auto accounts =
-            db->getStorage().get_all<Account>(where(c(&Account::accId) == accId));
+    auto accounts =
+        db->getStorage().get_all<Account>(where(c(&Account::accId) == accId));
 
-        // 조회할 계좌가 없음. 입금 실패
-        if (accounts.empty())
-            return false;
+    // 조회할 계좌가 없음. 입금 실패
+    if (accounts.empty())
+        return false;
 
-        if (accounts[0].cusId != cusId)
-            return false; // 권한없음. 입금 실패
+    if (accounts[0].cusId != cusId)
+        return false; // 권한없음. 입금 실패
 
-        accounts[0].balance += deposit_amount;
-        db->getStorage().update(accounts[0]);
-        MakeDepositLog(accounts[0], deposit_amount);
-        return true;
-    }
-    catch (std::exception &e)
-    {
-        db->handleException(e);
-    }
+    accounts[0].balance += deposit_amount;
+    db->getStorage().update(accounts[0]);
+    MakeDepositLog(accounts[0], deposit_amount);
+    return true;
 }
 
 bool AccountDB::WithdrawBalanceByAccId(std::string &accId,
@@ -154,42 +112,28 @@ bool AccountDB::WithdrawBalanceByAccId(std::string &accId,
                                        int cusId)
 {
     BankDatabase *db = BankDatabase::getInstance();
-    try
-    {
-        auto targetAcc =
-            db->getStorage().get_all<Account>(where(c(&Account::accId) == accId));
+    auto targetAcc =
+        db->getStorage().get_all<Account>(where(c(&Account::accId) == accId));
 
-        if (targetAcc.empty())
-            return false; // 계좌 조회 실패. 출금 실패.
+    if (targetAcc.empty())
+        return false; // 계좌 조회 실패. 출금 실패.
 
-        if (targetAcc[0].balance < withdraw_amount)
-            return false; // 잔액 부족. 출금 실패
+    if (targetAcc[0].balance < withdraw_amount)
+        return false; // 잔액 부족. 출금 실패
 
-        if (targetAcc[0].cusId != cusId)
-            return false; // 권한 없음. 출금 실패
+    if (targetAcc[0].cusId != cusId)
+        return false; // 권한 없음. 출금 실패
 
-        targetAcc[0].balance -= withdraw_amount;
-        db->getStorage().update(targetAcc[0]);
-        MakeWithdrawLog(targetAcc[0], withdraw_amount);
-        return true;
-    }
-    catch (std::exception &e)
-    {
-        db->handleException(e);
-    }
+    targetAcc[0].balance -= withdraw_amount;
+    db->getStorage().update(targetAcc[0]);
+    MakeWithdrawLog(targetAcc[0], withdraw_amount);
+    return true;
 }
 
 void AccountDB::UpdateAccount(Account &targetAcc)
 {
     BankDatabase *db = BankDatabase::getInstance();
-    try
-    {
-        db->getStorage().update(targetAcc);
-    }
-    catch (std::exception &e)
-    {
-        db->handleException(e);
-    }
+    db->getStorage().update(targetAcc);
 }
 
 void AccountDB::DeleteAccount(std::string accId)
@@ -197,18 +141,11 @@ void AccountDB::DeleteAccount(std::string accId)
     // transaction 외래키와의 종속성 때문에
     // 그냥 remove안하고 계좌 status를 false로
     BankDatabase *db = BankDatabase::getInstance();
-    try
-    {
-        auto targetAcc = db->getStorage().get_all<Account>(where(c(&Account::accId) == accId));
+    auto targetAcc = db->getStorage().get_all<Account>(where(c(&Account::accId) == accId));
 
-        if (targetAcc.empty())
-            return; // 삭제할 계좌 없음.
+    if (targetAcc.empty())
+        return; // 삭제할 계좌 없음.
 
-        targetAcc[0].status = false;
-        db->getStorage().update(targetAcc[0]);
-    }
-    catch (std::exception &e)
-    {
-        db->handleException(e);
-    }
+    targetAcc[0].status = false;
+    db->getStorage().update(targetAcc[0]);
 }
--- src/db/bank/models/customerdb.cc
+++ src/db/bank/models/customerdb.cc
@@ -7,74 +7,43 @@ CustomerDB::CustomerDB() {}
 Customer CustomerDB::CreateCustomer(std::string &name)
 {
     BankDatabase *db = BankDatabase::getInstance();
-    try
-    {
-        int insertedId = db->getStorage().insert(Customer{0, name});
-        Customer insertedCustomer = db->getStorage().get<Customer>(insertedId);
-        return insertedCustomer;
-    }
-    catch (std::exception &e)
-    {
-        db->handleException(e);
-    }
-} 
+    int insertedId = db->getStorage().insert(Customer{0, name});
+    Customer insertedCustomer = db->getStorage().get<Customer>(insertedId);
+    return insertedCustomer;
+}
 
 bool CustomerDB::isExistByCusId(int cusId)
 {
     BankDatabase *db = BankDatabase::getInstance();
-    try
-    {
-        auto customer = db->getStorage().get_all<Customer>(where(c(&Customer::cusId)==cusId));
-        if(customer.empty())
-            return false; //해당아이디의 고객 정보 없음.
-        return true;
-    }
-    catch (std::exception &e)
-    {
-        db->handleException(e);
-    }
+    auto customer = db->getStorage().get_all<Customer>(where(c(&Customer::cusId) == cusId));
+    if (customer.empty())
+        return false; // 해당아이디의 고객 정보 없음.
+    return true;
 }
 
-const Customer CustomerDB::GetCustomer(int cusId){
+const Customer CustomerDB::GetCustomer(int cusId)
+{
     BankDatabase *db = BankDatabase::getInstance();
-    try
-    {
-        auto customer = db->getStorage().get_all<Customer>(where(c(&Customer::cusId) == cusId));
-        if (customer.empty())
-            return {0, "\0"}; //고객 조회 실패.
-        return customer[0];
-    }
-    catch (std::exception &e)
-    {
-        db->handleException(e);
-    }
+    auto customer = db->getStorage().get_all<Customer>(where(c(&Customer::cusId) == cusId));
+    if (customer.empty())
+        return {0, "\0"}; // 고객 조회 실패.
+    return customer[0];
 }
 
-const Customer CustomerDB::GetCustomer(std::string& name){
-    BankDatabase * db = BankDatabase::getInstance();
-    try{
-        auto customer = db->getStorage().get_all<Customer>(where(c(&Customer::cus_name) == name));
-        if(customer.empty())
-            return {0, "\0"}; //고객 조회 실패. 
-        return customer[0];
-    }
-    catch(std::exception &e){
-        db->handleException(e);
-    }
+const Customer CustomerDB::GetCustomer(std::string &name)
+{
+    BankDatabase *db = BankDatabase::getInstance();
+    auto customer = db->getStorage().get_all<Customer>(where(c(&Customer::cus_name) == name));
+    if (customer.empty())
+        return {0, "\0"}; // 고객 조회 실패.
+    return customer[0];
 }
 
 void CustomerDB::UpdateCustomer(int cusId, std::string &changing_name)
 {
     BankDatabase *db = BankDatabase::getInstance();
-    try
-    {
-        db->getStorage().update_all(set(c(&Customer::cus_name) = changing_name),
-                                    where(c(&Customer::cusId) == cusId));
-    }
-    catch (std::exception &e)
-    {
-        db->handleException(e);
-    }
+    db->getStorage().update_all(set(c(&Customer::cus_name) = changing_name),
+                                where(c(&Customer::cusId) == cusId));
 }
 
 void CustomerDB::DeleteCustomer(int cusId)
@@ -82,13 +51,9 @@ void CustomerDB::DeleteCustomer(int cusId)
     // db에서 없애진 말자 transactionlog가 외래키로 잡혀있어서..
     // 고객에 딸린 account status false로
     BankDatabase *db = BankDatabase::getInstance();
-    try{
-        std::vector<Account> accounts = AccountDB::GetAccountsByCusId(cusId);
-        for(auto it = accounts.begin(); it!=accounts.end();++it){
-            it->status = false;
-        }
-    }
-    catch(std::exception &e){
-        db->handleException(e);
+    std::vector<Account> accounts = AccountDB::GetAccountsByCusId(cusId);
+    for (auto it = accounts.begin(); it != accounts.end(); ++it)
+    {
+        it->status = false;
     }
 }
