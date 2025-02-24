f2d48c47173f1211809394fcf5d607d7e5e38ce2
seungJoo <sj10230506@gmail.com>
2025-02-24 13:42:23 +0900

    feat: Customer header file 추가

---
+++ src/Server/models/Customer/customer_namespace.h
@@ -0,0 +1,11 @@
+
+#include <string>
+
+namespace customer{
+    typedef struct _customerInfo{
+        const unsigned int cusId;     //db auto increment
+        const std::string userId;   //로그인 아이디
+        std::string password; //로그인 비밀번호
+        std::string name;      
+    } CustomerInfo;
+}
