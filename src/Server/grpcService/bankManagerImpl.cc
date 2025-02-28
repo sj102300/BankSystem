bf0ef7d4493ca8c973661c63657fcbc6397d1067
SHLEE-235 <SHLEE-235@gmail.com>
2025-02-28 14:09:10 +0900

    grpc기능 추가

--- src/Server/grpcService/bankManagerImpl.cc
+++ src/Server/grpcService/bankManagerImpl.cc
@@ -1,4 +1,8 @@
 #include "bankManagerImpl.h"
+
+#include "customer_controller.h"
+#include "account_controller.h"
+
 #include <chrono>
 
 bankManagerImpl::bankManagerImpl()
@@ -7,6 +11,31 @@ bankManagerImpl::bankManagerImpl()
 
 bankManagerImpl::~bankManagerImpl()
 {
+}
+
+grpc::Status bankManagerImpl::signUp(grpc::ServerContext *context, const bank::customer::SignUpRequest *request, bank::customer::SignUpResponse *response)
+{
+    const auto [res, cus] = CustomerController::SignUp(request->userid(), request->password(), request->name());
+    response->set_success(res);
+    response->set_name(cus.name);
+    response->set_userid(cus.userId);
+
+    return grpc::Status();
+}
+
+grpc::Status bankManagerImpl::Login(grpc::ServerContext *context, const bank::customer::LogInRequest *request, bank::customer::LogInResponse *response)
+{
+    return grpc::Status();
+}
+
+grpc::Status bankManagerImpl::CreateFixedDepositAccount(grpc::ServerContext *context, const bank::account::CreateFixedDepositAccountRequest *request, bank::account::CreateFixedDepositAccountResponse *response)
+{
+    const auto res = AccountController::CreateFixedDepositAccount(request->cusid(), request->interestrate(), request->monthly_payment(), request->duedate());
+    response->set_accnum(res.ainfo.accNum_);
+    response->set_monthly_payment(res.finfo.monthlyPayment_);
+    response->set_duedate(res.finfo.maturityDate_);
+
+    return grpc::Status();
 }
 
 grpc::Status bankManagerImpl::deposit(grpc::ServerContext *context, const bank::DepositRequest *request, bank::DepositResponse *response)
--- src/Server/grpcService/bankManagerImpl.h
+++ src/Server/grpcService/bankManagerImpl.h
@@ -2,6 +2,9 @@
 #define BANK_MANAGER_IMPL_H
 
 #include "bank.grpc.pb.h"
+#include "account.grpc.pb.h"
+#include "customer.grpc.pb.h"
+
 #include <grpcpp/grpcpp.h>
 #include <memory>
 #include <string>
@@ -15,6 +18,11 @@ private:
 public:
   bankManagerImpl();
   ~bankManagerImpl();
+
+  grpc::Status signUp(grpc::ServerContext *context, const bank::customer::SignUpRequest *request, bank::customer::SignUpResponse *response);
+  grpc::Status Login(grpc::ServerContext *context, const bank::customer::LogInRequest *request, bank::customer::LogInResponse *response);
+
+  grpc::Status CreateFixedDepositAccount(grpc::ServerContext *context, const bank::account::CreateFixedDepositAccountRequest *request, bank::account::CreateFixedDepositAccountResponse *response);
 
   grpc::Status deposit(grpc::ServerContext *context, const bank::DepositRequest *request, bank::DepositResponse *response);
 };
