7d71583b5e9cbd611daeb8512d82b24c5f15a8c3
SHLEE-235 <SHLEE-235@gmail.com>
2025-03-04 15:45:48 +0900

    Modify : main 디렉토리 생성후 main.cc, services.cc .h이동

--- src/Server/services.h
+++
@@ -1,9 +0,0 @@
-#ifndef EDB89B9E_0FAD_4C89_9940_D31EC7B12420
-#define EDB89B9E_0FAD_4C89_9940_D31EC7B12420
-#include "bank.grpc.pb.h"
-#include <grpcpp/grpcpp.h>
-#include <iostream>
-
-void RunServer();
-
-#endif /* EDB89B9E_0FAD_4C89_9940_D31EC7B12420 */
